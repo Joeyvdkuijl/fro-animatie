aa26d32e6fd31fb53e2bd4f32e95105deaa7ddd7
Joey van der Kuijl <joeyvdkuijl@hotmail.com>
2020-12-07 11:08:38 +0100

    tussen commit

--- js/script.js
+++ js/script.js
@@ -5,3 +5,16 @@ document.querySelector('.animatie_knop').addEventListener('click', (e) => {
 setTimeout( () => {
     document.querySelector('.valkje').classList.toggle('vlakje__naar-rechts')  
 }, 2000 )
+
+
+const menuKnop = document.querySelector('.nav__knop');
+const navigatieItems = document.querySelectorAll('.nav__link');
+menuKnop.addEventListener('click', () => {
+    navigatieItems.forEach( (item,index)=> {
+        setTimeout( () => {
+            item.classList.toggle('nav__link--schuif-in');
+        }, 100 * index);
+    });
+    document.querySelector('.fa-bars').classList.toggle('verberg');
+    document.querySelector('.fa-times').classList.toggle('verberg');
+});
